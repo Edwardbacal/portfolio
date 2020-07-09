acd42d325ee885faad85c57bc6464f91374fd8e2
Edward Bacal <edwarddbacal@gmail.com>
2020-07-09 11:26:41 -0400

    added jQuery functions, coverted to SCSS

--- script.js
+++ script.js
@@ -15,6 +15,67 @@ $(".click").on("click", function () {
   $(this).children(".arrow").toggleClass("rotate");
 });
 
+
+$("#ceButton").on("click", function () {
+  console.log('yep');
+  $("#ceArt").fadeIn();
+});
+
+$("#bulldogButton").on("click", function () {
+  console.log('yep');
+  $("#bulldogBook").fadeIn();
+});
+
+$("#beerButton").on("click", function () {
+  console.log('yep');
+  $("#beer").fadeIn();
+});
+
+$("#psdButton").on("click", function () {
+  $("#psd").fadeIn();
+});
+
+
+const portfolio = {
+  "ce": `<div class="projectTitle">
+          <h5>CE Art Trace</h5>
+          <i class="fab fa-react"></i>
+          <a href=""><i class="fas fa-external-link-alt"></i></a>
+        </div>
+          <p class="clearP">An interactive interface for digital publications, presenting a more user-friendly alternative to
+          traditional e-texts.
+          </p>`,
+  "bulldog": "Bulldoggies!",
+  "beer": "burr",
+  "psd": "psddddddd",
+};
+
+
+$(".projectButton").on("click", function () {
+  const project = $(this).attr("id");
+  console.log(project);
+  $('.projectBox').empty();
+  $('.projectBox').fadeIn();
+  for (let projectTitle in portfolio) {
+    if (project === projectTitle) {
+      $('.projectBox').append("<h1>" + (portfolio[projectTitle]) + "</h1>");
+    }
+  }
+});
+
+// $(".footnote").on("click", function () {
+//   const number = $(this).attr("id");
+//   $('.note').empty();
+//   $('.note').toggle();
+//   for (let footnote in cite) {
+//     if (number === footnote) {
+//       $('.note').append("<p>" + (cite[footnote]) + "</p>");
+//     }
+//   }
+// });
+
+
+
 // $(".box img").on("mouseenter", function () {
 //   $(this).css("width", "100%");
 //   $(this).css("display", "block");
